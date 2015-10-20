8126439c18746afa4c830816c0c61fc94bf42491
dance2die <dance2die@hotmail.com>
2015-10-20 15:57:32 -0400

    Implemented AgencyParser

---
+++ Demo.GtfsTest/Demo.GtfsTest.Lib/Parsers/AgencyParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using Demo.GtfsTest.Lib.Core;
+
+namespace Demo.GtfsTest.Lib.Parsers
+{
+	public class AgencyParser
+	{
+		public IEnumerable<Agency> Parse(TextReader textReader)
+		{
+			var csv = new CsvReader(textReader);
+			csv.Configuration.RegisterClassMap<AgencyMap>();
+			csv.Configuration.WillThrowOnMissingField = false;
+			var records = csv.GetRecords<Agency>();
+
+			return records;
+		}
+	}
+}
